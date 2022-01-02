b76d5f76d9280e618e0591225ed9ffd86122d050
Ruma <th3rum2@gmail.com>
2022-01-02 00:51:39 -0500

    mejoras en el menu

---
+++ docs/js/menuButton.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector('#mobile-menu');
+
+menuButton.addEventListener('click', e =>{
+    const menu = document.querySelector('.mobile-links');
+
+    menu.classList.toggle('hidden');
+});
