aa3cd53c319fa61abffadc92fa5a38efcda67a7a
Ruma <th3rum2@gmail.com>
2022-01-03 15:09:33 -0500

    mejoras de fuentes y otra seccion en construccion

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,18 @@ module.exports = {
                 'character-mario': 'url(../assets/img/character-l.png)',
                 'pattern-characters-red': 'url(../assets/img/pattern-characters-red.png)',
                 'pattern-yellow-dots': 'url(../assets/img/pattern-yellow-dots.png)',
+                'pattern-red-dots': 'url(../assets/img/pattern-red-dots.png)',
                 'wave-pink': 'url(../assets/img/wave-pink.png)',
                 'pattern-black': 'url(../assets/img/pattern-characters-black.png)',
+                'pattern-white': 'url(../assets/img/pattern-characters-white.png)',
             }),
             screens:{
                 'xsm': '320px',
                 'xmd': '425px'
+            },
+            fontFamily:{
+                Barlow: "'Barlow',serif",
+                Libre:"'Libre Franklin', serif",
             }
         }
     },
