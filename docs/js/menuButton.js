e92948f4fc1e3c614fc537191615d04230aa0f7b
Rumazor <th3rum2084@gmail.com>
2022-05-07 12:36:50 -0500

    fix en el menu

--- docs/js/menuButton.js
+++ docs/js/menuButton.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector("#mobile-menu");
 
 menuButton.addEventListener("click", (e) => {
+  e.preventDefault();
   const menu = document.querySelector(".mobile-links");
 
   menu.classList.toggle("show-Links");
